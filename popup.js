3a9a652e868e7c197b873ed493bfcbf75b81a96b
Sam V <samyar.vahid@gmail.com>
2017-04-02 05:36:19 -0500

    i think it kinda works now

---
+++ popup.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+
+ 
+function parse(parser, doc)
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(stringContainingHTMLSource, "text/html");
+  if (input in doc) {
+    alert("TRIGGER WARNING REEEEEEEEE")
+  }
+  // returns an HTML document
